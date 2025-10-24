4fcba02a77c46fe94b40050904f8a9f03552ea38
wontop02 <wjy0502kr@naver.com>
2025-10-25 05:17:56 +0900

    test: 자동차 이름이 영어로만 구성되어 있지 않을 시 예외 발생 테스트 추가

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.domain.Car;
 
 public class CarTest {
@@ -16,4 +18,11 @@ public class CarTest {
                 .hasMessage("자동차 이름은 1자 이상, " + max + "자 이하로 입력해야 합니다.");
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"포비p", "pobi1"})
+    void 자동차_이름이_영어로만_구성되어_있지_않으면_예외가_발생한다(String name) {
+        assertThatThrownBy(() -> new Car(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름은 영어로만 입력해야 합니다.");
+    }
 }
