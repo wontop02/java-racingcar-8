cc81a8ffaf4a5568e5eeade163911af61121a58f
wontop02 <wjy0502kr@naver.com>
2025-10-25 17:10:31 +0900

    test: 시도할 횟수 입력값이 int 범위를 벗어났을 때 예외 발생 테스트 추가

--- src/test/java/racingcar/InputViewTest.java
+++ src/test/java/racingcar/InputViewTest.java
@@ -25,4 +25,12 @@ public class InputViewTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("시도할 횟수는 1-" + MAX_ATTEMPT_COUNT + " 사이의 정수로 입력해야 합니다.");
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"9999999999", "-9999999999"})
+    void 시도할_횟수는_int_범위를_벗어나면_예외가_발생한다(String input) {
+        assertThatThrownBy(() -> InputView.validateIntRange(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("시도할 횟수는 1-" + MAX_ATTEMPT_COUNT + " 사이의 정수로 입력해야 합니다.");
+    }
 }
