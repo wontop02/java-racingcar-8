d305fc5638ccd85e4dcf38285634f50b618dd204
wontop02 <wjy0502kr@naver.com>
2025-10-25 22:38:40 +0900

    test: RacingGame 매개변수 추가 및 모든 자동차의 전진 혹은 정지가 결정될 시 시도 횟수가 1 감소하는 기능 테스트 추가

--- src/test/java/racingcar/domain/RacingGameTest.java
+++ src/test/java/racingcar/domain/RacingGameTest.java
@@ -1,17 +1,26 @@
 package racingcar.domain;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
-import racingcar.domain.RacingGame;
 
 public class RacingGameTest {
     @Test
     void 중복된_자동차_이름이_존재하면_예외가_발생한다() {
         List<String> carNames = List.of("pobi", "woni", "pobi");
-        assertThatThrownBy(() -> new RacingGame(carNames))
+        assertThatThrownBy(() -> new RacingGame(carNames, 3))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("자동차 이름은 중복될 수 없습니다.");
+    }
+
+    @Test
+    void 모든_자동차의_전진_혹은_정지가_결정되면_시도_횟수가_1_감소한다() {
+        List<String> carNames = List.of("pobi", "woni");
+        int attemptCount = 3;
+        RacingGame racingGame = new RacingGame(carNames, attemptCount);
+        racingGame.moveCars();
+        assertEquals(racingGame.getAttemptCount(), 2);
     }
 }
