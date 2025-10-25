1cd4a1d92fc066a73a2c9af3f24efeb6d23c05c9
wontop02 <wjy0502kr@naver.com>
2025-10-26 00:24:25 +0900

    test: 자동차 이름에 공백이 존재할 경우 예외 발생 테스트 추가

--- src/test/java/racingcar/validator/CarValidatorTest.java
+++ src/test/java/racingcar/validator/CarValidatorTest.java
@@ -16,6 +16,13 @@ public class CarValidatorTest {
                 .hasMessage("자동차 이름은 중복될 수 없습니다.");
     }
 
+    @Test
+    void 자동차_이름에_공백이_존재할_경우_예외가_발생한다() {
+        assertThatThrownBy(() -> CarValidator.validateNoSpace("po bi"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("공백을 제외하고 입력해야 합니다.");
+    }
+
     @ParameterizedTest
     @ValueSource(strings = {"javaji", ""})
     void 자동차_이름은_1자_이상_max자_이하가_아니면_예외가_발생한다(String name) {
