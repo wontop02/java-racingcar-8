5c626572d9d891fee619410e65c14946174aec16
wontop02 <wjy0502kr@naver.com>
2025-10-25 15:19:18 +0900

    test: 자동차 이름이 중복될 경우 예외 발생 테스트 추가

---
+++ src/test/java/racingcar/RacingGameTest.java
@@ -0,0 +1,17 @@
+package racingcar;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import racingcar.domain.RacingGame;
+
+public class RacingGameTest {
+    @Test
+    void 중복된_자동차_이름이_존재하면_예외가_발생한다() {
+        List<String> carNames = List.of("pobi", "woni", "pobi");
+        assertThatThrownBy(() -> new RacingGame(carNames))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름은 중복될 수 없습니다.");
+    }
+}
