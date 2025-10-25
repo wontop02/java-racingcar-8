57777f2d6430800caace4b0226af4f0614865697
wontop02 <wjy0502kr@naver.com>
2025-10-25 22:01:35 +0900

    feat: 무작위 값이 4 이상일 때 전진하는 기능 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -6,11 +6,13 @@ import static racingcar.constant.GameRule.MAX_NAME_LENGTH;
 
 public class Car {
     private final String name;
+    private int position;
 
     public Car(String name) {
         validateNameLength(name);
         validateNameIsEnglish(name);
         this.name = name;
+        this.position = 0;
     }
 
     public void validateNameLength(String name) {
@@ -23,5 +25,15 @@ public class Car {
         if (!name.matches("^[a-zA-Z]*$")) {
             throw new IllegalArgumentException(INVALID_NAME_FORMAT);
         }
+    }
+
+    public void move(int randomValue) {
+        if (randomValue >= 4) {
+            position++;
+        }
+    }
+
+    int getPosition() {
+        return position;
     }
 }
