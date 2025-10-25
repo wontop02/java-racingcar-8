cc640f3ab2d99f838e1e8223b8cbfe7a6a04edcb
wontop02 <wjy0502kr@naver.com>
2025-10-26 03:04:45 +0900

    refactor: 불필요한 import 제거

--- src/test/java/racingcar/domain/RacingGameTest.java
+++ src/test/java/racingcar/domain/RacingGameTest.java
@@ -3,7 +3,6 @@ package racingcar.domain;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
