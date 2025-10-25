4f90ebd3721eb76730e3de6268ad0852f7b7d8a9
wontop02 <wjy0502kr@naver.com>
2025-10-25 15:25:41 +0900

    test: 빈 문자열 테스트 케이스 추가

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -2,18 +2,17 @@ package racingcar;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
-import java.util.List;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.domain.Car;
 
 public class CarTest {
 
-    @Test
-    void 자동차_이름은_1자_이상_max자_이하가_아니면_예외가_발생한다() {
+    @ParameterizedTest
+    @ValueSource(strings = {"javaji", ""})
+    void 자동차_이름은_1자_이상_max자_이하가_아니면_예외가_발생한다(String name) {
         int max = 5;
-        assertThatThrownBy(() -> new Car("javaji"))
+        assertThatThrownBy(() -> new Car(name))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("자동차 이름은 1자 이상, " + max + "자 이하로 입력해야 합니다.");
     }
