c123cb2e9caf8d123a7e246242045761815f1e65
wontop02 <wjy0502kr@naver.com>
2025-10-25 16:27:46 +0900

    test: 테스트 이름 및 예외 메시지 변경

--- src/test/java/racingcar/InputViewTest.java
+++ src/test/java/racingcar/InputViewTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.view.InputView;
 
 public class InputViewTest {
+    static final int MAX_ATTEMPT_COUNT = 30;
     @ParameterizedTest
     @ValueSource(strings = {"pobi", "a,b,c,d,e,f,g,h"})
     void 자동차_개수는_min_이상_max_이하가_아니면_예외가_발생한다(String input) {
@@ -19,9 +20,9 @@ public class InputViewTest {
     }
 
     @Test
-    void 시도할_횟수는_정수가_아니면_예외가_발생한다() {
+    void 시도할_횟수는_숫자를_제외한_문자가_포함되면_예외가_발생한다() {
         assertThatThrownBy(() -> InputView.parseAttemptCount("1.5"))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("시도할 횟수는 정수로 입력해야 합니다.");
+                .hasMessage("시도할 횟수는 1-" + MAX_ATTEMPT_COUNT + " 사이의 정수로 입력해야 합니다.");
     }
 }
