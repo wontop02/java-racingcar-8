eb3d54f5791b9d4799fe411d82d2e6ce490ca236
wontop02 <wjy0502kr@naver.com>
2025-10-25 22:40:00 +0900

    feat: 모든 자동차 이동 및 시도 횟수 감소 기능 구현

--- src/main/java/racingcar/domain/RacingGame.java
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -2,26 +2,38 @@ package racingcar.domain;
 
 import static racingcar.constant.ErrorMessage.DUPLICATED_CAR_NAME;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.HashSet;
 import java.util.List;
 
 public class RacingGame {
     private final List<Car> cars;
+    private int attemptCount;
 
-    public RacingGame(List<String> carNames) {
+    public RacingGame(List<String> carNames, int attemptCount) {
         cars = createCars(carNames);
+        this.attemptCount = attemptCount;
     }
 
-    public List<Car> createCars(List<String> carNames) {
+    private List<Car> createCars(List<String> carNames) {
         validateDuplicateNames(carNames);
         return carNames.stream()
                 .map(Car::new)
                 .toList();
     }
 
-    public void validateDuplicateNames(List<String> carNames) {
+    private void validateDuplicateNames(List<String> carNames) {
         if (carNames.size() != new HashSet<>(carNames).size()) {
             throw new IllegalArgumentException(DUPLICATED_CAR_NAME);
         }
+    }
+
+    public void moveCars() {
+        cars.forEach(car -> car.move(Randoms.pickNumberInRange(0, 9)));
+        attemptCount--;
+    }
+
+    int getAttemptCount() {
+        return attemptCount;
     }
 }
